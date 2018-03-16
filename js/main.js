2a1a399261f555592d5530c29a23b9b6b26c74ce
unknown <dani.d.zamorano@gmail.com>
2018-03-16 16:48:30 +0100

    Añadido pluquin jQuery para animar los numeros de  resumen-evento

--- js/main.js
+++ js/main.js
@@ -178,4 +178,11 @@ $(function(){
     return false;
   });
 
+  //Animaciones numeros
+
+  $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
+  $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1300);
+  $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1400);
+  $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1500);
+
 });
