b5337e2d560e626a9f8a46a6a57af25233968682
unknown <dani.d.zamorano@gmail.com>
2018-03-17 14:15:55 +0100

    barra de navegación fija + ajustes estilo barra

--- js/main.js
+++ js/main.js
@@ -173,10 +173,23 @@ $(function(){
 
   // Menú fijo
 
+  var windowHeight = $(window).height();
+  var barraAltura = $('.barra').innerHeight();
+
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
 
-    console.log(scroll);
+      if(scroll > windowHeight){
+        $('.barra').addClass('fixed');
+        $('body').css({
+          'margin-top': barraAltura +'px'
+        });
+      }else{
+        $('.barra').removeClass('fixed');
+        $('body').css({
+          'margin-top': 0 +'px'
+        });
+      }
     
   });
 
