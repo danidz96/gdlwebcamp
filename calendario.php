d18f42a76c1c764d40b7d0bbb8ec4133736900d9
unknown <dani.d.zamorano@gmail.com>
2018-04-02 00:48:56 +0200

    imprimir todos los eventos

--- calendario.php
+++ calendario.php
@@ -7,7 +7,7 @@
         <?php
         try {
           require_once('includes/funciones/bd_conexion.php');
-          $sql = "SELECT id_evento, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado FROM eventos
+          $sql = "SELECT id_evento, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado FROM eventos
           INNER JOIN categoria_evento ON eventos.id_cat_evento = categoria_evento.id_categoria INNER JOIN invitados ON eventos.id_inv = invitados.id_invitado ORDER BY id_evento";
           $resultado = $db->query($sql);
         } catch (\Exception $e) {
@@ -25,15 +25,37 @@
                   'fecha' => $eventos['fecha_evento'],
                   'hora' => $eventos['hora_evento'],
                   'categoria' => $eventos['cat_evento'],
+                  'icono' => 'fa' . " " . $eventos['icono'],
                   'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']);
 
                 $calendario[$eventos['fecha_evento']][] = $evento;
                 ?>
             <?php } ?>
+            <?php
+            // Imprime todos los $eventos
+            foreach ($calendario as $dia => $lista_eventos) { ?>
+              <h3>
+                <i class='fa fa-calendar'></i>
+                <?php
+                setlocale(LC_TIME, 'spanish');
+                echo date("F j, Y", strtotime($dia)); ?>
+              </h3>
 
-            <pre>
-              <?php var_dump($calendario); ?>
-            </pre>
+              <?php foreach ($lista_eventos as $evento){ ?>
+                <div class="dia">
+                  <p class="titulo"><?php echo $evento['titulo']; ?></p>
+                  <p class="hora"><i class="fa fa-clock" aria-hidden="true"></i><?php echo $evento['fecha'] . " " . $evento['hora'];  ?></p>
+                  <p>
+                    <i class="<?php echo $evento['icono']; ?>" aria-hidden="true"></i>
+                    <?php echo $evento['categoria']; ?>
+                  </p>
+                  <p><i class="fa fa-user" aria-hidden="true"></i><?php echo $evento['invitado'];?></p>
+
+                </div>
+
+              <?php } ?>
+          <?php } ?>
+
 
         </div>
         <?php
