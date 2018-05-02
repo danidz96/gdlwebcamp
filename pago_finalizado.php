92fabb1d59166a74a57cda7ef2e1a1bfc36432c9
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-05-02 18:11:52 +0200

    cambiando estado registro si fue correcto el pago

--- pago_finalizado.php
+++ pago_finalizado.php
@@ -7,9 +7,17 @@
 
       if ($resultado == "true") {
         $paymentId = $_GET['paymentId'];
-        $id_pago = $_GET['id_pago'];
+        $id_pago = (int)$_GET['id_pago'];
         echo "El resultado se realizo correctamente </br>";
         echo "El ID es {$paymentId}";
+
+        require_once('includes/funciones/bd_conexion.php');
+        $stmt = $db->prepare("UPDATE registrados SET pagado = ? WHERE id_registrado = ? ");
+        $pagado = 1;
+        $stmt->bind_param("ii", $pagado, $id_pago);
+        $stmt->execute();
+        $stmt->close();
+        $db->close();
       }else {
         echo "El pago no se realizó";
       }
