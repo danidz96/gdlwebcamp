5334c66be93da84d8462a5c3fe964200d06fdff4
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-05-02 11:39:16 +0200

    Añadiendo archivos para hacer pagos con PayPal

---
+++ pagar.php
@@ -0,0 +1,73 @@
+<?php
+
+if (!isset($_POST['producto'], $_POST['precio'])) {
+  exit("Algo ha ido mal");
+}
+
+use PayPal\Api\Payer;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Details;
+use PayPal\Api\Amount;
+use PayPal\Api\Transaction;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Payment;
+
+require 'paypal.php';
+
+$producto = htmlspecialchars($_POST['producto']);
+$precio = htmlspecialchars($_POST['precio']);
+$precio = (int) $precio;
+$envio = 0;
+$total = $precio + $envio;
+
+$compra = new Payer();
+$compra->setPaymentMethod('paypal');
+
+$articulo = new item();
+$articulo->setName($producto)
+         ->setCurrency('EUR')
+         ->setQuantity(1)
+         ->setPrice($precio);
+
+$listaArticulos = new ItemList();
+$listaArticulos->setItems(array($articulo));
+$detalles = new Details();
+$detalles->setShipping($envio)
+         ->setSubtotal($precio);
+
+$cantidad = new Amount();
+$cantidad->setCurrency('EUR')
+         ->setTotal($precio)
+         ->setDetails($detalles);
+
+$transaccion = new Transaction();
+$transaccion->setAmount($cantidad)
+            ->setItemList($listaArticulos)
+            ->setDescription('Pago ')
+            ->setInvoiceNumber(uniqid());
+
+$redireccionar = new RedirectUrls();
+$redireccionar->setReturnUrl(URL_SITIO . "/pago_finalizado.php?exito=true")
+              ->setCancelUrl(URL_SITIO. "/pago_finalizado.php?exito=false");
+
+$pago = new Payment();
+$pago->setIntent("sale")
+     ->setPayer($compra)
+     ->setRedirectUrls($redireccionar)
+     ->setTransactions(array($transaccion));
+
+try {
+  $pago->create($apiContext);
+} catch (Paypal\Exception\PaypalConnectionException $pce) {
+  echo "<pre>";
+  print_r(json_decode($pce->getData()));
+  exit;
+  echo "<pre>";
+}
+
+$aprobado = $pago->getApprovalLink();
+
+header("Location: {$aprobado}");
+
+ ?>
--- registro.php
+++ registro.php
@@ -2,7 +2,7 @@
 
     <section class="seccion contenedor">
         <h2>Registro de usuarios</h2>
-        <form  id="registro" class="registro" action="validar_registro.php" method="post">
+        <form  id="registro" class="registro" action="pagar.php" method="post">
             <div id="datos-usuarios" class="registro caja clearfix">
                 <div class="campo">
                     <label for="nombre">Nombre: </label>
