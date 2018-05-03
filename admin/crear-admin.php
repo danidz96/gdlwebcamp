1d9c0fdfcfdf6060fa58a225642527457ebe2b8a
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-05-03 12:26:42 +0200

    creando el formulario para crear admin

---
+++ admin/crear-admin.php
@@ -0,0 +1,62 @@
+<?php
+include_once'templates/header.php';
+include_once'templates/barra.php';
+include_once'templates/aside.php';
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Crear Administrador
+        <small>Rellena el formulario</small>
+      </h1>
+    </section>
+
+    <div class="row">
+      <div class="col-md-8">
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Crear Admin</h3>
+
+        </div>
+        <div class="box-body">
+          <!-- form start -->
+          <form role="form" name="crear-admin" id="crear-admin" method="post" action="insertar-admin.php">
+            <div class="box-body">
+              <div class="form-group">
+                <label for="usuario">Usuario</label>
+                <input type="email" class="form-control" id="usuario" name="usuario" placeholder="Usuario">
+              </div>
+              <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input type="email" class="form-control" id="nombre" name="nombre" placeholder="Nombre">
+              </div>
+              <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+              </div>
+            </div>
+            <!-- /.box-body -->
+
+            <div class="box-footer">
+              <button type="submit" class="btn btn-primary">Añadir</button>
+            </div>
+          </form>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+      </div>
+    </div>
+  </div>
+  <!-- /.content-wrapper -->
+  <?php include_once'templates/footer.php'; ?>
