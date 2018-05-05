6555d57c8a9d1c66c3995ed36d4a121d99856b57
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-05-05 19:11:05 +0200

    eliminando eventos

--- admin/modelo-evento.php
+++ admin/modelo-evento.php
@@ -7,21 +7,23 @@ if($db->ping()){
 }*/
 include_once 'funciones/funciones.php';
 
-$titulo = $_POST['titulo_evento'];
-$categoria_id = $_POST['categoria_evento'];
-$invitado_id = $_POST['invitado'];
-// obtener la fecha
-$fecha = $_POST['fecha_evento'];
-$fecha_formateada = date('Y-m-d', strtotime($fecha));
 
-// hora
-$hora = $_POST['hora_evento'];
-$hora_formateada = date('H:i', strtotime($hora));
-
-$id_registro = $_POST['id_registro'];
 
 if($_POST['registro'] == 'nuevo'){
+  $titulo = $_POST['titulo_evento'];
+  $categoria_id = $_POST['categoria_evento'];
+  $invitado_id = $_POST['invitado'];
+  // obtener la fecha
+  $fecha = $_POST['fecha_evento'];
+  $fecha_formateada = date('Y-m-d', strtotime($fecha));
+
+  // hora
+  $hora = $_POST['hora_evento'];
+  $hora_formateada = date('H:i', strtotime($hora));
+
+  $id_registro = $_POST['id_registro'];
     try {
+        include_once 'funciones/funciones.php';
         $stmt = $db->prepare("INSERT INTO eventos (nombre_evento, fecha_evento, hora_evento, id_cat_evento, id_inv) VALUES ( ?, ?, ?, ? ,? )  ");
         $stmt->bind_param('sssii', $titulo, $fecha_formateada, $hora_formateada, $categoria_id, $invitado_id);
         $stmt->execute();
@@ -39,18 +41,26 @@ if($_POST['registro'] == 'nuevo'){
         $stmt->close();
         $db->close();
     } catch (Exception $e) {
-        $respuesta = array(
-            'respuesta' => $e->getMessage()
-        );
+        echo "Error: " . $e->getMessage;
+
     }
     die(json_encode($respuesta));
 
 }
 
 if($_POST['registro'] == 'actualizar'){
+  $titulo = $_POST['titulo_evento'];
+  $categoria_id = $_POST['categoria_evento'];
+  $invitado_id = $_POST['invitado'];
+  // obtener la fecha
+  $fecha = $_POST['fecha_evento'];
+  $fecha_formateada = date('Y-m-d', strtotime($fecha));
 
+  // hora
+  $hora = $_POST['hora_evento'];
+  $hora_formateada = date('H:i', strtotime($hora));
     try {
-        $stmt = $db->prepare('UPDATE eventos SET nombre_evento = ?, fecha_evento = ?, hora_evento = ?, id_cat_evento = ?, id_inv = ?, editado = NOW() WHERE id_event = ? ');
+        $stmt = $db->prepare('UPDATE eventos SET nombre_evento = ?, fecha_evento = ?, hora_evento = ?, id_cat_evento = ?, id_inv = ?, editado = NOW() WHERE id_evento = ? ');
         $stmt->bind_param('sssiii', $titulo, $fecha_formateada, $hora_formateada, $categoria_id, $invitado_id, $id_registro );
         $stmt->execute();
         if($stmt->affected_rows) {
@@ -78,7 +88,28 @@ if($_POST['registro'] == 'actualizar'){
 
 if($_POST['registro'] == 'eliminar'){
 
+    $id_borrar = $_POST['id'];
 
+    try {
+        $stmt = $db->prepare('DELETE FROM eventos WHERE id_evento = ? ');
+        $stmt->bind_param('i', $id_borrar);
+        $stmt->execute();
+        if($stmt->affected_rows) {
+            $respuesta = array(
+                'respuesta' => 'exito',
+                'id_eliminado' => $id_borrar
+            );
+        } else {
+            $respuesta = array(
+                'respuesta' => 'error'
+            );
+        }
+    } catch (Exception $e) {
+        $respuesta = array(
+            'respuesta' => $e->getMessage()
+        );
+    }
+    die(json_encode($respuesta));
 }
 
 
